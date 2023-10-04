5413c048fa2e4af92935faf9ebf5b78a7994048a
ipogodin <illia.pogodin@gmail.com>
2023-10-04 13:18:27 -0700

    updated the activation process, updated readme
    added activation into the popup
    added reactivation in 3 secons
    added documentation to the readme

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,27 @@
-const switchElement = document.getElementById('doublePressSwitch');
+const togglePluginButton = document.getElementById('togglePlugin');
+const restoreOverlayButton = document.getElementById('restoreOverlay');
 
-const storageArea = chrome.storage.sync || chrome.storage.local;
-
-storageArea.get({ 'isDoublePressActive': true }, ({ isDoublePressActive }) => {
-    switchElement.checked = isDoublePressActive;
+// Initial state check
+chrome.storage.sync.get({ 'isPluginActive': true }, ({ isPluginActive }) => {
+    togglePluginButton.textContent = isPluginActive ? 'Deactivate Plugin' : 'Activate Plugin';
 });
 
-switchElement.addEventListener('change', (e) => {
-    storageArea.set({ 'isDoublePressActive': e.target.checked });
+togglePluginButton.addEventListener('click', () => {
+    chrome.storage.sync.get({ 'isPluginActive': true }, ({ isPluginActive }) => {
+        const newState = !isPluginActive;
+        togglePluginButton.textContent = newState ? 'Deactivate Plugin' : 'Activate Plugin';
+        chrome.storage.sync.set({ 'isPluginActive': newState });
+
+        // Inform content script about the plugin's state
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { isPluginActive: newState });
+        });
+    });
+});
+
+restoreOverlayButton.addEventListener('click', () => {
+    // Inform content script to restore overlay
+   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'restoreOverlay' });
+    });
 });
