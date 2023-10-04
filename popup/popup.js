bf49a1899ca101cad30808de928348f7aceb16dd
ipogodin <illia.pogodin@gmail.com>
2023-10-04 14:48:16 -0700

    updated the popup look
    added the high coupling between popup and overlay
    added images to popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,48 @@
-const togglePluginButton = document.getElementById('togglePlugin');
-const restoreOverlayButton = document.getElementById('restoreOverlay');
+const togglePluginIcon = document.getElementById('togglePluginIcon');
+const restoreOverlayIcon = document.getElementById('restoreOverlayIcon');
+const pathToActivatePluginImg = "images/active_lsg.png";
+const pathToDeactivatePluginImg = "images/inactive_lsg.png"
+const pathToReactivateImg = "images/reactivate.png"
 
 // Initial state check
 chrome.storage.sync.get({ 'isPluginActive': true }, ({ isPluginActive }) => {
-    togglePluginButton.textContent = isPluginActive ? 'Deactivate Plugin' : 'Activate Plugin';
+     togglePluginIcon.src = isPluginActive ? pathToActivatePluginImg : pathToDeactivatePluginImg;
+
+      if (isPluginActive) {
+         restoreOverlayIcon.style.pointerEvents = 'auto';  // Icon is clickable
+         restoreOverlayIcon.style.opacity = '1';           // Full opacity
+     } else {
+         restoreOverlayIcon.style.pointerEvents = 'none'; // Icon is non-clickable
+         restoreOverlayIcon.style.opacity = '0.5';        // Reduced opacity for visual cue
+     }
 });
 
-togglePluginButton.addEventListener('click', () => {
+togglePluginIcon.addEventListener('click', () => {
     chrome.storage.sync.get({ 'isPluginActive': true }, ({ isPluginActive }) => {
         const newState = !isPluginActive;
-        togglePluginButton.textContent = newState ? 'Deactivate Plugin' : 'Activate Plugin';
+        togglePluginIcon.src = newState ? pathToActivatePluginImg : pathToDeactivatePluginImg;
         chrome.storage.sync.set({ 'isPluginActive': newState });
 
         // Inform content script about the plugin's state
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { isPluginActive: newState });
         });
+
+        if (newState) {
+            restoreOverlayIcon.style.pointerEvents = 'auto';  // Icon is clickable
+            restoreOverlayIcon.style.opacity = '1';           // Full opacity
+        } else {
+            restoreOverlayIcon.style.pointerEvents = 'none'; // Icon is non-clickable
+            restoreOverlayIcon.style.opacity = '0.5';        // Reduced opacity for visual cue
+        }
     });
 });
 
-restoreOverlayButton.addEventListener('click', () => {
+restoreOverlayIcon.addEventListener('click', () => {
     // Inform content script to restore overlay
    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'restoreOverlay' });
     });
 });
+
+
